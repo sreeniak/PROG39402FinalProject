9b975486d31e71594a13b77e5ef03989376e8a99
Nadin Azraq <azraqn@sheridancollege.ca>
2023-11-29 20:26:59 -0500

    partial setup for workout

---
+++ app/src/main/java/akshita/ken/nadin/WorkoutAdapter.kt
@@ -0,0 +1,8 @@
+package akshita.ken.nadin
+
+import akshita.ken.nadin.Data.Workout.Workout
+import androidx.recyclerview.widget.RecyclerView
+
+class WorkoutAdapter {
+
+}
---
+++ app/src/main/java/akshita/ken/nadin/WorkoutViewModel.kt
@@ -0,0 +1,24 @@
+package akshita.ken.nadin
+
+import akshita.ken.nadin.Data.Workout.Workout
+import akshita.ken.nadin.Data.Workout.WorkoutDao
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class WorkoutViewModel(private val workoutDao: WorkoutDao) : ViewModel() {
+
+    val allworkouts: LiveData<List<Workout>> = workoutDao.getAllWorkouts()
+
+
+    suspend fun addWorkout(workout: Workout) {
+        workoutDao.addWorkout(workout)
+    }
+
+    suspend fun updateWorkout(workout: Workout) {
+        workoutDao.updateWorkout(workout)
+    }
+
+    suspend fun deleteWorkout(workout: Workout) {
+        workoutDao.deleteWorkout(workout)
+    }
+}

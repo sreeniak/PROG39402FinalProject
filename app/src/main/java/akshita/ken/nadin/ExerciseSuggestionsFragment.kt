ad3230daf43c7cecef65eca6a3c841fa26d8f895
Ken <kenflorencio@gmail.com>
2023-12-04 22:05:44 -0500

    Finished base for home page - with 3 buttons to workout + diet lists, and exercise suggestions page for webview

--- app/src/main/java/akshita/ken/nadin/ExerciseSuggestionsFragment.kt
+++ app/src/main/java/akshita/ken/nadin/ExerciseSuggestionsFragment.kt
@@ -1,5 +1,6 @@
 package akshita.ken.nadin
 
+import akshita.ken.nadin.databinding.FragmentExerciseSuggestionsBinding
 import akshita.ken.nadin.databinding.FragmentWelcomeBinding
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,23 +9,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 
-class WelcomeFragment : Fragment() {
-    private var _binding: FragmentWelcomeBinding? = null
+class ExerciseSuggestionsFragment : Fragment() {
+    private var _binding: FragmentExerciseSuggestionsBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        _binding = FragmentExerciseSuggestionsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnWelcomeToHome.setOnClickListener {
-            findNavController().navigate(R.id.action_welcomeFragment_to_homeFragment)
-        }
+//        binding.btnSuggestionsToWorkoutList.setOnClickListener {
+//            findNavController().navigate(R.id.)
+//        }
     }
+
 }
--- app/src/main/java/akshita/ken/nadin/HomeFragment.kt
+++ app/src/main/java/akshita/ken/nadin/HomeFragment.kt
@@ -1,17 +1,41 @@
 package akshita.ken.nadin
 
+import akshita.ken.nadin.databinding.FragmentHomeBinding
+import akshita.ken.nadin.databinding.FragmentWelcomeBinding
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnHomeToWorkoutList.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_workoutListFragment)
+        }
+
+        binding.btnHomeToDietJournal.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_mealListFragment)
+        }
+
+        binding.btnHomeToExerciseSuggestions.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_exerciseSuggestionsFragment)
+        }
+
+
     }
 }

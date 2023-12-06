939f223eef47e84933f815f25c446d7b391f7df6
sreeniak <127881258+sreeniak@users.noreply.github.com>
2023-12-06 11:34:33 -0500

    success message

--- app/src/main/java/akshita/ken/nadin/EditMealFragment.kt
+++ app/src/main/java/akshita/ken/nadin/EditMealFragment.kt
@@ -86,6 +86,7 @@ class EditMealFragment : Fragment() {
             )
 
             viewModel.updateMeal(newMeal, requireContext())
+            Toast.makeText(requireContext(), "Meal Updated", Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(requireContext(), "Please enter calories", Toast.LENGTH_SHORT).show()
         }
